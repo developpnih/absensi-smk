9899876c9f4f46553084284cd2b78fcb1652ed87
developpnih <ramadhanii9702@gmail.com>
2026-07-09 15:07:06 +0700

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,5 @@
 // GANTI dengan URL Web App hasil deploy Google Apps Script (Code.gs)
 // Contoh: https://script.google.com/macros/s/AKfycb.../exec
-const API_URL = 'https://script.google.com/macros/s/AKfycbyIMbUuZsHmFjAY78-xgAnYA0BnhYq2J3vrP5S_cENVKt3ThRTS9lh1J3jf9GNHkSkudg/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbyh2ii9VouJrDCy6SL6_fYQCLwW75yXnNkn6-xcC4EeIKCAuBKi0eLLHFslYFf1Zpo7xw/exec';
 
 const NAMA_SEKOLAH = 'SMK Muhammadiyah Bojong';
